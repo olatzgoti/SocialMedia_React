6fa1b17a976ff2c602cb5d588d1e6f0aeb9efa84
olatz <gotiolatz@gmail.com>
2024-03-02 12:53:12 +0100

    login+header_basic+routes

--- frontend_React/redux-intro/src/App.jsx
+++ frontend_React/redux-intro/src/App.jsx
@@ -1,17 +1,17 @@
-
-//import { Register } from './components/Register/Register.jsx'
+import TheHeader from './components/Header/Header'
+import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import  Register from './components/Register/Register'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
-
- // <Route path='/login' element={<Login/>}/>
   return (
     <div className='App'>
       <BrowserRouter>
+      <TheHeader/>
         <Routes>
+            <Route path='/home' element={<Home/>}/>
             <Route path='/register' element={<Register/>}/>
             <Route path='/login' element={<Login/>}/>
         </Routes>  
---
+++ frontend_React/redux-intro/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const TheHeader = () => {
+    return (
+        <div>
+            <h1> Header </h1>
+        </div>
+    )
+}
+
+export default TheHeader
